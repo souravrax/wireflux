a33a4836563205ddfe2cdd5e853b0a5047ec6239
Sourav Rakshit <rakshitsourav3@gmail.com>
2025-08-01 14:56:23 +0530

    feat: add example project with React, TypeScript, and Vite setup
    - Created a new example project structure with React and TypeScript.
    - Added ESLint configuration for code quality and consistency.
    - Included basic Vite configuration for development and build processes.
    - Implemented a sample App component with state management and styling.
    - Added API error handling and fetch client for API interactions.
    - Introduced .gitignore to exclude unnecessary files and directories.
    - Updated package.json with necessary dependencies and scripts for development.

--- example/wireflux.config.ts
+++ example/wireflux.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'wireflux';
 
 export default defineConfig({
   input: './openapi.json', // Path to your OpenAPI schema
-  targetFolder: './generated', // Where to generate the client
-  fetchClient: './super-fetch', // Your fetch client implementation
-  apiError: './api-error', // Your API error class
+  targetFolder: './src/api/operations', // Where to generate the client
+  fetchClient: './src/api/fetchClient', // Your fetch client implementation
+  apiError: './src/api/api-error', // Your API error class
   baseUrl: 'http://localhost:3000/api',
   supportedMethods: ['get', 'post', 'put', 'delete', 'patch'],
 });
